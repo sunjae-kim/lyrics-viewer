34451b351deb423f972990314138fe65ce92c50c
Jason <kimsj9484@gmail.com>
2021-05-23 00:48:27 +0900

    Set cors whitelist

--- functions/index.js
+++ functions/index.js
@@ -5,27 +5,37 @@ const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
 
+const whitelist = ['http://localhost:3000', 'https://sunjae.kim'];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
 const SEARCH_URL =
   'https://apis.naver.com/vibeWeb/musicapiweb/v3/search/track?sort=RELEVANCE&query=';
 const GET_TRACK_INFO_URL = trackId =>
   `https://apis.naver.com/vibeWeb/musicapiweb/track/${trackId}/info`;
 
 app.use(helmet());
-app.use(cors());
 app.use(morgan('combined'));
 
 app.get('/', (req, res) => {
   res.send('server is alive');
 });
 
-app.get('/getTrackList', async (req, res) => {
+app.get('/getTrackList', cors(corsOptions), async (req, res) => {
   const { query } = req.query;
   const response = await axios.get(encodeURI(SEARCH_URL + query));
   const trackList = response.data.response.result.tracks;
   res.send(trackList);
 });
 
-app.get('/getTrackLyrics', async (req, res) => {
+app.get('/getTrackLyrics', cors(corsOptions), async (req, res) => {
   const { trackId } = req.query;
   const response = await axios.get(GET_TRACK_INFO_URL(trackId));
   res.send(response.data.response.result.trackInformation);
