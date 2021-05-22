9f774fd621d82a39734628db3bb4ca3f845bdb6c
Jason <kimsj9484@gmail.com>
2021-05-22 20:02:31 +0900

    firebase hosting

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,20 +1,20 @@
 import colors from 'vuetify/es5/util/colors';
 
 export default {
-  mode: 'universal',
   /*
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Lyrics Viewer',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || '',
-      },
+      { name: 'description', content: 'Music lyrics viewer' },
+      { name: 'author', content: 'Jason' },
+      { property: 'og:title', content: 'Lyrics Viewer' },
+      { property: 'og:type', content: 'website' },
+      { property: 'og:url', content: 'lyrics.sunjae.kim' },
+      { property: 'og:description', content: 'Music lyrics viewer' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
